11ea8729692be1fdf1769a8378442371e4737941
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2023-04-04 10:32:41 +0530

    latest-changes

--- index.js
+++ index.js
@@ -34,12 +34,29 @@ app.use(bodyParser.raw());
 app.use(cookieParser());
 app.use(adminRoutes);
 
+// const config = {
+//   server: "N1NWPLSK12SQL-v03.shr.prod.ams1.secureserver.net",
+//   user: "aasha",
+//   password: "2!Oryc83",
+//   port: 1433,
+//   database: "aasha-bsvwithu",
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     idleTimeoutMillis: 30000,
+//   },
+//   options: {
+//     encrypt: true, // for azure
+//     trustServerCertificate: true, // change to true for local dev / self-signed certs
+//   },
+// };
+
 const config = {
-  server: "N1NWPLSK12SQL-v03.shr.prod.ams1.secureserver.net",
-  user: "aasha",
-  password: "2!Oryc83",
+  server: "P3NWPLSK12SQL-v15.shr.prod.phx3.secureserver.net",
+  user: "spakDb",
+  password: "Spak@123-",
   port: 1433,
-  database: "aasha-bsvwithu",
+  database: "bsvDb",
   pool: {
     max: 10,
     min: 0,
